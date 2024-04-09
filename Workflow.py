52161f8b6310717cb906ad23a06d6fb786d10377
Juanlyjack <junjielee815@gmail.com>
2024-04-10 00:05:47 +0800

    update plt

--- Workflow.py
+++ Workflow.py
@@ -84,20 +84,48 @@ class Workflow(object):
         save_path = os.path.join(save_dir, name)
         Workflow.__check_dir(save_path)
         for i, samples in enumerate(sample_list):
-            file_path = os.path.join(save_path, f"{name}-{samples[0].mid_idx}.jpg")
-            total = len(samples)
-            plt.figure(figsize=(12, 10))
-            for j, sample in enumerate(samples):
-                # sample.show_plot()
-                plt.subplot(total // 2, 2, j + 1)
-                # plt.ylim(-10*1e-5, 10*1e-5)
-                plt.ylim(sample.min * 1.5, sample.max * 1.5)
-                plt.xlim(0, 2000)
-                plt.xticks([0, 500, 1000, 1500, 2000], ['0', '1', '2', '3', '4'])
-                plt.plot(sample.sample, scalex=False, scaley=False)
-                plt.title(f"{sample.channel_name}_{j + 1}")
-                # sample.sub_plot((total, 1, j + 1))
-            plt.savefig(file_path)
+            try:
+                file_path = os.path.join(save_path, f"{name}-{samples[0].mid_idx}.jpg")
+                total = len(samples)
+                total_seconds = samples[0].time_length // 1000
+                x_ticks_point = [i * 500 for i in np.arange(0, total_seconds + 0.5, 0.5)]
+                x_ticks_label = [str(i) for i in np.arange(0, total_seconds + 0.5, 0.5)]
+                plt.figure(figsize=(12, 10))
+                for j, sample in enumerate(samples):
+                    # sample.show_plot()
+                    plt.subplot(total // 2, 2, j + 1)
+                    # plt.ylim(-10*1e-5, 10*1e-5)
+                    y_min = sample.min * 1.5 * 1000000
+                    y_max = sample.max * 1.5 * 1000000
+                    y_min_label = int(y_min) // 10 * 10
+                    y_max_label = int(y_max) // 10 * 10
+                    plt.ylim(y_min, y_max)
+                    if y_min_label >= -80 and y_max_label <= 80:
+                        plt.yticks([y_min_label, -50, 0, 50, y_max_label], [str(y_min_label), '-50', '0', '50', str(y_max_label)])
+                    elif y_min_label >= -80 and y_max_label > 80:
+                        plt.yticks([y_min_label, -50, 0, 50, 80, y_max_label], [str(y_min_label), '-50', '0', '50', '80', str(y_max_label)])
+                    elif y_min_label < -80 and y_max_label <= 80:
+                        plt.yticks([y_min_label, -80, -50, 0, 50, y_max_label], [str(y_min_label), '-80', '-50', '0', '50', str(y_max_label)])
+                    else:
+                        plt.yticks([y_min_label, -80, -50, 0, 50, 80, y_max_label], [str(y_min_label), '-80', '-50', '0', '50', '80', str(y_max_label)])
+                    plt.gca().invert_yaxis()
+                    # plt.xlim(0, 2000)
+                    plt.xlim(0, sample.length)
+                    data = sample.sample * 1000000
+                    # plt.xticks([0, 500, 1000, 1500, 2000], ['0', '1', '2', '3', '4'])
+                    plt.xticks(x_ticks_point, x_ticks_label)
+                    plt.plot(data, scalex=False, scaley=False)
+                    plt.grid(True, which='both', axis='both', linestyle='--', linewidth=0.5, color='lightgrey')
+                    # 添加浅色的零线
+                    plt.axhline(0, color='lightgrey', linewidth=0.5)  # 添加y=0水平线
+                    plt.xlabel('Time (s)')
+                    plt.ylabel('Amplitude (μV)')
+                    plt.title(f"{sample.channel_name}_{j + 1}")
+                    t=0
+                    # sample.sub_plot((total, 1, j + 1))
+                plt.savefig(file_path)
+            except:
+                print(f"###### ERROR sample is {i} ######")
 
 
     @staticmethod
